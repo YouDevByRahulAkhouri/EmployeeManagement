f14dca061e4897c55e785b06c8c9febd484ffad9
Menka <menka.kumari@ivycomptech.com>
2021-01-05 02:43:09 +0000

    initial commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,39 +1,52 @@
-import React , { useState } from "react";
+import React from "react";
 import "./LoginForm.css";
 
 class LoginForm extends React.Component {
-  componentDidMount() {
-    const apiUrl = " http://127.0.0.1:5000/lms/loginAdmin";
-    fetch(apiUrl,payload)
-      .then((response) => response.json())
-      .then((data) => console.log("This is your data", data));
-
-    const [state, setState] = useState({
-        email: "",
-        password: "",
-        successMessage: null,
-      });
-    const handleChange = (e) => {
-        const { id, value } = e.target;
-        setState((prevState) => ({
-          ...prevState,
-          [id]: value,
-        }));
-      };
-    
-    const handleSubmitClick = (e) => {
-        e.preventDefault();
-        const payload = {
-          email: state.email,
-          password: state.password,
-        };
-    
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
   }
-  
-  render() 
-  {
+  handleChange = (e) => {
+    const { id, value } = e.target;
+    console.log(id, value)
+    this.setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  handleSubmitClick = (e) => {
+    e.preventDefault();
+    alert('submitting')
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    const headers = {
+      Authorization: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOjEyMzR9.AkulsG22blITRUe4-iROKG25EIqT8H2-5HXLT93nQXc'
+    }
+    const options = {
+      method: 'POST',
+      headers: headers,
+      body: payload
+    }
+    const apiUrl = " http://127.0.0.1:5000/lms/admin";
+    fetch(apiUrl, options)
+      .then((res) => res.json())
+      .then((data) => this.setState({ response: data.response, success: data.success }));
+
+  }
+
+  render() {
+    console.log(this.state)
     return (
       <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+        {this.state.success === false && <p style={{ color: 'red' }}>Something went wrong.Try again!</p>}
+        {this.state.response && <p>{this.state.response}</p>}
         <form>
           <div className="form-group text-left">
             <label htmlFor="exampleInputEmail1">Email address</label>
@@ -42,12 +55,23 @@ class LoginForm extends React.Component {
               className="form-control"
               id="email"
               placeholder="Enter email"
-              value={state.email}
-              onChange={handleChange}
+              value={this.state.email}
+              onChange={this.handleChange}
             />
             <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
             </small>
+          </div>
+          <div className="form-group text-left">
+            <label htmlFor="exampleInputPassword1">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-group text-left">
             <label htmlFor="exampleInputPassword1">Password</label>
@@ -56,20 +80,21 @@ class LoginForm extends React.Component {
               className="form-control"
               id="password"
               placeholder="Password"
-              value={state.password}
-              onChange={handleChange}
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-check"></div>
           <button
             type="submit"
             className="btn btn-primary"
-            onClick={handleSubmitClick}
+            onClick={this.handleSubmitClick}
           >
             Submit
           </button>
         </form>
-       
+
+
         <div className="registerMessage">
           <span>Dont have an account? </span>
           <span className="loginText">
@@ -79,6 +104,6 @@ class LoginForm extends React.Component {
       </div>
     );
   }
-  
+
 }
 export default LoginForm;
